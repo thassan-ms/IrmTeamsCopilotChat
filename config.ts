012d2903b564376452bc67bd61d161f59305a644
Diego Ruvalcaba <diruvalc@microsoft.com>
2023-09-11 14:38:08 -0700

    Initial setup for teams app

---
+++ config.ts
@@ -0,0 +1,9 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  openAIKey: process.env.SECRET_OPENAI_API_KEY,
+  openAIKey2: process.env.OPENAI_KEY2,
+  openAIEndpoint: process.env.OPENAI_ENDPOINT
+};
+
+export default config;
